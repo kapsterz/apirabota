ae55140f0c5e1f2fba235b16b06ca2a9efb7a224
kapsterz <wegodeboy@yandex.com>
2017-03-20 18:53:48 +0200

    Api for processing URL and JQuery-like path, and receive Json with elements.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,26 +1,37 @@
 package controllers
 
-import java.io.File
-
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
 import play.api._
 import play.api.mvc._
 import javax.inject._
-
+import actors.FileParserActor.ResultFileRow
 import actors.FileParserActor
-import actors.FileParserActor.GetFileToParse
-import akka.actor._
-import play.api.libs.json.Json
+import play.api.data.Forms._
+import play.api.data.Form
+import play.api.data.Forms.mapping
+import play.api.libs.json.JsValue
 
 import scala.concurrent.ExecutionContext
 
 
 class Application @Inject()(fileParserActor: FileParserActor)(implicit executionContext: ExecutionContext) extends Controller {
-  def index = Action.async {
-    Logger.info("success send message for process file " + new File("new.txt"))
-    val result = fileParserActor.parseFile(new File("new.txt"))
-    result.map{ f=>
-      Ok(Json.obj("all" -> f))
-    }
+  private val userForm = Form(
+    mapping(
+      "requestUrl" -> text,
+      "requestXPath" -> text
+    )(ResultFileRow.apply)(ResultFileRow.unapply)
+  )
+  def main = Action{
+    Ok(views.html.main(userForm))
+  }
+  def index: Action[ResultFileRow] = Action.async(parse.form(userForm)) {
+    implicit request =>
+      Logger.info("Success received message for process json")
+      val result = fileParserActor.parseJson(request.body)
+      result.map { f =>
+        Ok(f)
+      }
   }
 
 }
